ba1947a9db66c6e4c49035b7a2af2486d38aa0c1
LarisaNichitoaia <139768565+LarisaNichitoaia@users.noreply.github.com>
2023-08-09 16:03:14 +0300

    second commit

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "tt-bucket" {
-  bucket = "tt-bucket"
+  bucket = var.bucket_name
 }
 
 resource "aws_s3_object" "my-first-obj" {
--- variables.tf
+++ variables.tf
@@ -1,3 +1,7 @@
+variable "bucket_name"{
+  type = string,
+  default = "tt-bucket"
+}
 variable "file_assets" {
   type = map(string)
 }
